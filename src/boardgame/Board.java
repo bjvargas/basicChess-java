67411e93e0658153e31ddd86f65ebd2bfc00bc50
Bruno Jaques <bruno_jaques@hotmail.com>
2020-09-29 18:08:31 -0300

    Adicionada exceções

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,43 +8,72 @@ public class Board {
 	
 	
 	public Board(int rows, int columns) {
+		
+		this.tamanhoMinimo(rows, columns);
+		
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
-	}
-
+	}	
+	
 	public Piece piece(int row, int column) {
+		this.posicaoInexistente(row, column);
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
+		this.posicaoInexistente(position);
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		this.posicaoOcupada(position);
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
 	
+	private boolean positionExists(int row, int column) {
+		return row >=0 && row < rows && column >= 0 && column < columns;
+	}
 	
+	public boolean positionExists(Position position) {
+		return this.positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		this.posicaoInexistente(position);
+		return piece(position) != null;
+	}
+	
+	private void tamanhoMinimo(int rows, int columns) {
+		if(rows <1 || columns < 1) {
+			throw new BoardException("Erro na criação do tabuleiro: Precisa ter no minimo 1 linha e 1 coluna.");
+		}
+	}
+	
+	private void posicaoInexistente(int row, int column) {
+		if(!this.positionExists(row, column)) {
+			throw new BoardException("Posição não existe no tabuleiro.");
+		}
+	}
+
+	private void posicaoInexistente(Position position) {
+		if(!this.positionExists(position)) {
+			throw new BoardException("Posição não existe no tabuleiro.");
+		}
+	}
+	
+	private void posicaoOcupada(Position position) {
+		if(this.thereIsAPiece(position)) {
+			throw new BoardException("Já existe uma peça na posição: " + position);
+		}
+	}
 
 	public int getRows() {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
-
 	public int getColumns() {
 		return columns;
 	}
-
-
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
-
 }
